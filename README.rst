7bbea71ec17fdad81c09c5bc811145c162368626
antonioIrizar <antonioirizar@gmail.com>
2019-11-10 17:51:40 +0100

    init django app

---
+++ README.rst
@@ -0,0 +1,2 @@
+# django-check-migration-wtf
+check migrations of django to work on enviroment zero downtime
---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='django-check-migration-wtf',
+    version='0.1',
+    packages=find_packages(),
+    description='A line of description',
+    long_description=README,
+    author='Antonio Irizar',
+    author_email='antonioirizar@gmail.com',
+    url='https://github.com/antonioIrizar/django-check-migration-wtf',
+    license='GLP3',
+    python_requires='>=3.7',
+    install_requires=[
+        'Django>=2.2,<2.3',
+    ]
+)

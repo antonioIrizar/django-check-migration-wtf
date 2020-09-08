df07abc58db2783efeaa1aee42a9d8bae0ee4238
Antonio <antonioirizar@gmail.com>
2020-09-08 16:38:04 +0200

    add support for django 3.1 (#45)
    * add support for django 3.1
    
    * fix tox

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
     ],
     keywords='django postgres postgresql migrations',
     python_requires='>=3.6',

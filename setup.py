85e3abb5af7ef4be0b6c3d2644212f7055218e5f
antonioIrizar <antonioirizar@gmail.com>
2022-12-16 13:14:31 +0100

    upgrade version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='django-check-migration-wtf',
-    version='1.3.1',
+    version='1.4.0',
     packages=find_packages(),
     description='A line of description',
     long_description=README,
